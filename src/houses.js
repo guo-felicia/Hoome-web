ebb2c7aab7be4177fb06b5ff27018d91f1183947
tongfeiguo <tongfei.gf@gmail.com>
2022-04-11 11:18:38 -0400

    add house data

---
+++ src/houses.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "handle": "NASA",
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "likes": 345,
+        "dislikes": 65,
+        "avatar":"/img/avatar/nasa.jpg"
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "handle": "NASA",
+        "tuit": "Ingenuity helicopter takes flight.",
+        "likes": 346785,
+        "dislikes": 239,
+        "avatar":"/img/avatar/nasa.jpg"
+    }
+];
