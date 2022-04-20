d637702ae530f261ec53b9a34884b8e5f285888c
tongfeiguo <tongfei.gf@gmail.com>
2022-04-20 15:27:31 -0400

    a

--- src/components/search/SearchPage.js
+++ src/components/search/SearchPage.js
@@ -22,7 +22,7 @@ const SearchPage = () => {
         }
     };
     
-    useEffect(() => fetchMe(), [term])
+    useEffect(() => fetchMe(), [])
     
     const fetchMe = () => {
         
@@ -39,7 +39,7 @@ const SearchPage = () => {
         <div>
             {
                 container.map((house) =>
-                    <a href=""
+                    <a href="/search/${house.id}"
                         onClick={() => {
                         navigate(`/search/${house.id}`, {state: {house}})
                     }}>
