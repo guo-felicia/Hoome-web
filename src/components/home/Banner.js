a6a1b105e2fff40b5a737708e00d1a5856780734
tongfeiguo <tongfei.gf@gmail.com>
2022-04-21 21:19:32 -0400

    delete unused import

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../../style/Banner.css'
 import { Button } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
