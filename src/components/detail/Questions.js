eed364da3377fe6b526f568067abf1389fe06753
Yuqi1124 <yuemengjun@gmail.com>
2022-04-30 21:33:12 -0400

    display delete conditionally

--- src/components/detail/Questions.js
+++ src/components/detail/Questions.js
@@ -10,6 +10,8 @@ const Questions = () => {
     const {profile} = useProfile()
     const question = useSelector(
         state => state.questions);
+    console.log('questions')
+    console.log(question)
     // create a new state variable with new tuit data
     const [newQuestion, setNewQuestion] =
         useState({question: 'New Question'});
@@ -31,9 +33,9 @@ const Questions = () => {
         <div className='question-container'>
             {/*TODO log-in check, true - display this text entering area*/}
             {/*TEXT ENTERING AREA*/}
+            <h2 className="question-title">Frequently Ask</h2>
             <SecureContent>
                 <div className="question-box">
-                    <h2 className="question-title">Frequently Ask</h2>
             <textarea className="text-box"
                 placeholder="Type your questions here"
                 onChange={(e) =>
@@ -52,47 +54,58 @@ const Questions = () => {
             </SecureContent>
             
             {
-                question.map && question.map(question =>
-                    <div className='question-block grid-row'>
-                        <div className="grid-col-left-sidebar bg-color-yellow">
-                            <Link to={`/profile/${question.postedBy.username}`}>
-                            <img className="wd-avatar" src={question["avatar"]} alt="Avatar"/>
-                            </Link>
-                            <p className="wd-handel-15px">{question.postedBy.firstName}</p>
-                            <h3 className="wd-margintop-0">{question.question}</h3>
-                        </div>
-                        <div className="grid-col-main-content bg-color-blue fg-color-white">
-                            <p className="white">.</p>
-                        </div>
-                        <div className="grid-col-right-sidebar bg-color-green fg-color-white action-box">
-                            <div className="like">
-                                <p className="center icon-font hover">
-                                    <i onClick={() => updateQuestion(dispatch, {
-                                    ...question,
-                                    likes: question.likes + 1
-                                    })} className="far fa-thumbs-up icon"/>
-                                    {question.likes}
-                                </p>
+                question.map && question.map((question) => {
+                    let canDelete = false;
+                    if (profile) {
+                        if (question.postedBy.username === profile.username) {
+                            canDelete = true;
+                        }
+                    }
+                    console.log(canDelete);
+                    return (
+                        <div className='question-block grid-row'>
+                            <div className="grid-col-left-sidebar bg-color-yellow">
+                                <Link to={`/profile/${question.postedBy.username}`}>
+                                    <img className="wd-avatar" src={question["avatar"]} alt="Avatar"/>
+                                </Link>
+                                <p className="wd-handel-15px">{question.postedBy.firstName}</p>
+                                <h3 className="wd-margintop-0">{question.question}</h3>
                             </div>
-                            <div className="dislike">
-                                <p className="center icon-font hover">
-                                    <i onClick={() => updateQuestion(dispatch, {
-                                    ...question,
-                                    dislikes: question.dislikes + 1
-                                    })} className="far fa-thumbs-down icon"/>
-                                    {question.dislikes}
-                                </p>
+                            <div className="grid-col-main-content bg-color-blue fg-color-white">
+                                <p className="white">.</p>
                             </div>
-                            {/*TODO log-in check, current user posted this post (can use userName) if yes: display delete button*/}
-                            <div className="delete">
-                                <p className="center icon-font hover">
-                                    <i className="fas fa-trash float-end"
-                                    onClick={() => deleteQuestion(
-                                    dispatch, question)}/>
-                                </p>
+                            <div className="grid-col-right-sidebar bg-color-green fg-color-white action-box">
+                                <div className="like">
+                                    <p className="center icon-font hover">
+                                        <i onClick={() => updateQuestion(dispatch, {
+                                            ...question,
+                                            likes: question.likes + 1
+                                        })} className="far fa-thumbs-up icon"/>
+                                        {question.likes}
+                                    </p>
+                                </div>
+                                <div className="dislike">
+                                    <p className="center icon-font hover">
+                                        <i onClick={() => updateQuestion(dispatch, {
+                                            ...question,
+                                            dislikes: question.dislikes + 1
+                                        })} className="far fa-thumbs-down icon"/>
+                                        {question.dislikes}
+                                    </p>
+                                </div>
+                                {/*TODO log-in check, current user posted this post (can use userName) if yes: display delete button*/}
+                                {canDelete && <div className="delete">
+                                    <p className="center icon-font hover">
+                                        <i className="fas fa-trash float-end"
+                                           onClick={() => deleteQuestion(
+                                               dispatch, question)}/>
+                                    </p>
+                                </div>}
                             </div>
                         </div>
-                    </div>
+                    )
+                }
+
                 )
             }
         </div>);
